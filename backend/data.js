fa1547be7840d6fa5e25b4b09df2c41a2c126677
Luke Gartland <lukegartland@gmail.com>
2021-08-11 14:56:43 -1000

    servering api data to front end

--- backend/data.js
+++ backend/data.js
@@ -1,40 +1,91 @@
-const data = [
-    {
-        pageName: 'home',
-       content: [
+// const data = [
+//     {
+//         pageName: 'home',
+//        content: [
+//             {title: 'breath in', img: '../images/greylogo.png', subTitle: 'sub title'},
+//             {directions: 'breath out', img: '../images/greylogo.png'},
+//             {title: 'relax', img: '../images/greylogo.png'},
+//             {title: 'scroll', img: '../images/greylogo.png'}
+//         ]
+//     },
+//     {
+//         pageName: 'fear',
+//       content: [
+//             {title: 'fear', img: '../images/greylogo.png', subTitle: 'sub title'},
+//             {directions: 'breath out', img: '../images/greylogo.png'},
+//             {title: 'relax', img: '../images/greylogo.png'},
+//             {title: 'scroll', img: '../images/greylogo.png'}
+//         ]
+//     },
+//     {
+//         pageName: 'nsfw',
+//        content: [
+//             {title: 'NSFW', img: '../images/greylogo.png', subTitle: 'sub title'},
+//             {directions: 'breath out', img: '../images/greylogo.png'},
+//             {title: 'relax', img: '../images/greylogo.png'},
+//             {title: 'scroll', img: '../images/greylogo.png'}
+//         ]
+//     },
+//     {
+//         pageName: 'affirmations',
+//         content: [
+//             {title: 'affirmations', img: '../images/greylogo.png', subTitle: 'sub title'},
+//             {directions: 'breath out', img: '../images/greylogo.png'},
+//             {title: 'relax', img: '../images/greylogo.png'},
+//             {title: 'scroll', img: '../images/greylogo.png'}
+//         ]
+//     }
+// ]
+
+
+
+
+
+// module.exports = data
+
+
+
+// const home = [
+//             {title: 'breath in', img: '../images/greylogo.png', subTitle: 'sub title'},
+//             {directions: 'it works', img: '../images/greylogo.png'},
+//             {title: 'relax', img: '../images/greylogo.png'},
+//             {title: 'scroll', img: '../images/greylogo.png'}
+//         ]
+
+// module.exports = home
+
+
+
+
+
+const data = {
+    
+       home: [
             {title: 'breath in', img: '../images/greylogo.png', subTitle: 'sub title'},
             {directions: 'breath out', img: '../images/greylogo.png'},
             {title: 'relax', img: '../images/greylogo.png'},
             {title: 'scroll', img: '../images/greylogo.png'}
-        ]
-    },
-    {
-        pageName: 'fear',
-      content: [
+            ],
+   
+    
+        fear: [
             {title: 'fear', img: '../images/greylogo.png', subTitle: 'sub title'},
             {directions: 'breath out', img: '../images/greylogo.png'},
             {title: 'relax', img: '../images/greylogo.png'},
             {title: 'scroll', img: '../images/greylogo.png'}
-        ]
-    },
-    {
-        pageName: 'nsfw',
-       content: [
+            ],
+        nsfw: [
             {title: 'NSFW', img: '../images/greylogo.png', subTitle: 'sub title'},
             {directions: 'breath out', img: '../images/greylogo.png'},
             {title: 'relax', img: '../images/greylogo.png'},
             {title: 'scroll', img: '../images/greylogo.png'}
-        ]
-    },
-    {
-        pageName: 'affirmations',
-        content: [
+            ],
+        affirmations: [
             {title: 'affirmations', img: '../images/greylogo.png', subTitle: 'sub title'},
             {directions: 'breath out', img: '../images/greylogo.png'},
             {title: 'relax', img: '../images/greylogo.png'},
             {title: 'scroll', img: '../images/greylogo.png'}
         ]
-    }
-]
+   }
 
-module.exports = data
+   module.exports = data
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+require('dotenv').config();
 const data = require('./data');
+// const home = require('./data');
 const app = express()
 
 app.get('/', (req, res) => {
@@ -10,9 +12,12 @@ app.get('/api/data', (req, res) => {
     res.json(data)
 })
 
+app.get('/api/data/home', (req, res) => {
+    res.json(home)
+})
+
 app.get('/api/data/:page', (req, res) => {
-    const page = data.find(p => p.pageName === req.params.page)
-    res.json(page)
+    res.json(data[req.params.page])
 })
 
 app.listen(5000, console.log('server running on port 5000'))
