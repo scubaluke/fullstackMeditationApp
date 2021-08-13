83717faefa0a2b7b407ebc6e0a6b56117d93b039
Luke Gartland <lukegartland@gmail.com>
2021-08-13 06:40:57 -1000

    content at component level working

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-// import axios from 'axios'
+import axios from 'axios'
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 import CoreStyles from 'react-awesome-slider/src/core/styles.scss';
@@ -8,24 +8,25 @@ import AnimationStyles from 'react-awesome-slider/src/styled/fold-out-animation/
 import { listContent } from '../actions/contentActions'
 
 export default function Home({ match }) {
-  // const [content, setContent] = useState([])
-  const dispatch = useDispatch()
-  const content = useSelector(state => state.content)
-  const {loading, error, mantras} = content
-  console.log("content", content);
+  const [content, setContent] = useState([])
+  // const dispatch = useDispatch()
+  // const content = useSelector(state => state.content)
+  // const {loading, error, mantras} = content
+  // console.log("content", content);
 
       useEffect(() => {
-        //   const fetchData = async () => {
-        //     const { data } = await axios.get(`/api/content/${match.url.substring(1)}`)
+          const fetchData = async () => {
+            const { data } = await axios.get(`/api/content/${match.url.substring(1)}`)
 
-        //     setContent(data)
-        //   }
-        // fetchData()
-        dispatch(listContent(match.url.substring(1)))
+            setContent(data)
+          }
+        fetchData()
+        
+        // dispatch(listContent(match.url.substring(1)))
         console.log('use effect ran');
-      }, [match, dispatch])
+      }, [match])
 
-         const renderPage = mantras.map((mantra, i) => {
+         const renderPage = content.map((mantra, i) => {
               return <div key={i} style={{backgroundImage: `url(${mantra.img})`}} >
                       {mantra.pageTitle && <h1 className="box-title">{mantra.pageTitle}</h1>}
                       {mantra.subTitle && <h2 className='sub-title' >{mantra.subTitle}</h2>}
