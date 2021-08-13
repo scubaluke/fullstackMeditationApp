e535738f7d7c31361eef0d1e1b18fe7e85d31872
Luke Gartland <lukegartland@gmail.com>
2021-08-13 10:29:38 -1000

    user auth backend routes cset up and working

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,5 +46,40 @@ export const getUserProfile = asyncHandler(async (req, res) => {
         res.status(404)
         throw new Error('User not found')
     }
-    
+})
+
+
+// @desc register new User
+// @route POST api/users 
+// @access public
+export const registerUser = asyncHandler(async (req, res) => {
+        const { name, email, phone, password } = req.body
+        const insureEmail = email || ''
+        
+    const userExists = await User.findOne({ email: insureEmail.toLowerCase() })
+
+    if(userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name, email: insureEmail.toLowerCase(), phone, password
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            phone: user.phone,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+   
+
 })
