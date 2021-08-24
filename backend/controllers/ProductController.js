a5aefee8db5ff855303ccfe997039e9eadf40b35
Luke Gartland <lukegartland@gmail.com>
2021-08-24 11:19:05 -1000

    get product by id route

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -8,3 +8,14 @@ export const getProducts = asyncHandler(async (req, res) => {
    const products = await Product.find({})
    res.json(products)
 })
+
+// @desc get product by id
+// @route get api/products/:id 
+// @access public
+export const getProductById = asyncHandler(async (req, res) => {
+   const id = req.params.id 
+   
+   const product = await Product.findOne({ _id: id })
+
+   res.json(product)
+})
