070ebfa72ac749623f4a48148789325da65971f6
Luke Gartland <lukegartland@gmail.com>
2021-08-23 21:30:22 -1000

    ready to add cart component and functions

--- frontend/src/pages/ShopPage.js
+++ frontend/src/pages/ShopPage.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import Login from '../components/Login';
-import Shop from '../components/ProductList';
+// import Login from '../components/Login';
 import Loader from '../components/Loader';
 import Message from '../components/Message'
 import { listProducts } from '../actions/productActions';
@@ -11,8 +10,7 @@ export default function ShopPage() {
     const dispatch = useDispatch()
     const products = useSelector(state => state.products)
     const {loading, data, error} = products
-    console.log('shopPage', products);
-
+    
     useEffect(() => {
         
       dispatch(listProducts())
