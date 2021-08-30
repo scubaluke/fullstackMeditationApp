86ba24f80a9623188e6c6de4a7991115b2b2f367
Luke Gartland <lukegartland@gmail.com>
2021-08-30 07:59:38 -1000

    route handler for update user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,6 +48,36 @@ export const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc update user profile
+// @route PUT api/users/profile 
+// @access private
+export const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user){
+       user.name = req.body.name || user.name
+       user.email = req.body.email || user.email
+       user.phone = req.body.phone || user.phone
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+        const updatedUser = await user.save()
+
+           res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            phone: updatedUser.phone,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id)
+        }) 
+
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
 
 // @desc register new User
 // @route POST api/users 
