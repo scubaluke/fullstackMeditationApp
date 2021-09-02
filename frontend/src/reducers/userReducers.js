156971e81c42c0e6e6c0ddd5149d2700b8aae7cf
Luke Gartland <lukegartland@gmail.com>
2021-09-01 14:50:27 -1000

    form re-render on update

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -37,9 +37,7 @@ export const userDetailsReducer = (state = { user: {} } , action) => {
         case USER_DETAILS_SUCCESS:
             return   { loading: false, user: action.payload }
         case USER_DETAILS_FAIL:
-            return { loading: false, error: action.payload }  
-        // case USER_LOGOUT:
-        //     return {}  
+            return { loading: false, error: action.payload }   
         default:
             return state
     }
